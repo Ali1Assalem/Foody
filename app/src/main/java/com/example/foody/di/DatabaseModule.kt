2843ff93149e139624f5b935893e713aa374c9c0
ALI <ali11salem00@gmail.com>
2022-07-27 23:26:45 +0300

    Create and Inject LocalDataSource & DatabaseModule

---
+++ app/src/main/java/com/example/foody/di/DatabaseModule.kt
@@ -0,0 +1,33 @@
+package com.example.foody.di
+
+
+import android.content.Context
+import androidx.room.Room
+import com.example.foody.data.database.RecipesDatabase
+import com.example.foody.util.Constants.Companion.DATABASE_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(
+        context,
+        RecipesDatabase::class.java,
+        DATABASE_NAME
+    ).build()
+
+    @Singleton
+    @Provides
+    fun provideDao(database: RecipesDatabase) = database.recipesDao()
+
+}
