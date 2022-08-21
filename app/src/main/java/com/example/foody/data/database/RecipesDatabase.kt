12b2d85d89d5f30f75b4662c49e6af67f7c752cc
ALI <ali11salem00@gmail.com>
2022-08-21 13:27:11 +0300

    FoodJokeFragment&Binding&Update Room-MainViewModel

--- app/src/main/java/com/example/foody/data/database/RecipesDatabase.kt
+++ app/src/main/java/com/example/foody/data/database/RecipesDatabase.kt
@@ -4,10 +4,11 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.example.foody.data.database.entities.FavoritesEntity
+import com.example.foody.data.database.entities.FoodJokeEntity
 import com.example.foody.data.database.entities.RecipesEntity
 
 @Database(
-    entities = [RecipesEntity::class, FavoritesEntity::class],
+    entities = [RecipesEntity::class, FavoritesEntity::class, FoodJokeEntity::class],
     version = 1,
     exportSchema = false
 )
